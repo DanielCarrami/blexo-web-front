2c33290f3ba9f7ab581ccde05e9be719a47bd850
alaricomv <alarico.mercado@gmail.com>
2020-09-15 20:07:22 -0500

    Agregue pagina ver-sesion

---
+++ src/app/ver-sesion/ver-sesion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-sesion',
+  templateUrl: './ver-sesion.component.html',
+  styleUrls: ['./ver-sesion.component.scss']
+})
+export class VerSesionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
