c9edbed432d8991d71626532429c005922a3d7e9
Jacqueline Ortiz <jos@localhost.localdomain>
2020-09-23 08:59:00 -0500

    Registro coded #2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VerSesionComponent } from './ver-sesion/ver-sesion.component';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
   {path: 'versesion', component: VerSesionComponent},
+  {path: 'registro', component: RegistroComponent},
 ];
 
 @NgModule({
