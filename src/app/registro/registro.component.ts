df0e18fdc42275c621c6b64e3aa7f5217b34618d
jaorsa <A01329978@itesm.mx>
2020-10-05 00:36:32 -0500

    correcciones

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.scss']
 })
 export class RegistroComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Debes ingresar un valor';
+    }
+
+    return this.email.hasError('email') ? 'No es un correo electronico valido' : '';
+  }
 
   constructor() { }
 
