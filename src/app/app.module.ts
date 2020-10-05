34ce32264eb79fe74a9c3d88a357f8bcb2517b0d
jaorsa <A01329978@itesm.mx>
2020-10-05 01:37:51 -0500

    eeeeeesssshhhh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { VerSesionComponent } from './ver-sesion/ver-sesion.component';
 import { ChartsModule } from 'ng2-charts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EditarSesionComponent } from './editar-sesion/editar-sesion.component';
+
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import {MatFormField, MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
+
 import { CrearSesionComponent } from './crear-sesion/crear-sesion.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 import { RegistroComponent } from './registro/registro.component';
 import { HeaderComponent } from './core/header/header.component';
 
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
+ //import {FormsModule} from '@angular/forms';
+// import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -36,13 +39,13 @@ import { MatButtonModule } from '@angular/material/button';
     ChartsModule,
     MatDialogModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
+    MatFormField,
+    //MatFormFieldModule,
     MatInputModule,
     MatToolbarModule,
-    MatIconModule,
-    FormsModule, 
-    ReactiveFormsModule,
-    MatButtonModule
+    MatIconModule
+     //FormsModule
+    // MatButtonModule
   ],
   entryComponents:[EditarSesionComponent, VerSesionComponent, CrearSesionComponent],
   providers: [],
