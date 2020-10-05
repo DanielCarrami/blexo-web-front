52f3d6a7df5ec3eb13e47e221289f01ae9c94c2b
jaorsa <A01329978@itesm.mx>
2020-10-05 11:11:54 -0500

    changes

--- src/app/editar-usuario/editar-usuario.component.ts
+++ src/app/editar-usuario/editar-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-usuario',
@@ -6,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-usuario.component.scss']
 })
 export class EditarUsuarioComponent implements OnInit {
+  name = new FormControl('', [Validators.required, Validators.pattern((/^[a-zA-Z]*$/))]);
+  lastname = new FormControl('', [Validators.required, Validators.pattern((/^[a-zA-Z]*$/))  ]);
+  // phone = new FormControl('', [Validators.required, Validators.pattern((/^((\\+91-?)|0)?[0-9]{10}$/))]);
+  // password = new FormControl('', [Validators.required]);
+  // confirmpassword = new FormControl('', [Validators.required]);
+
+
+  // email = new FormControl('', [Validators.required, Validators.email]);
+  // hide = true;
+  // hideconfirm = true;
+
+  getNameErrorMessage() {
+    if (this.name.hasError('required')) {
+     return 'Debes ingresar un nombre';
+   }
+
+   return this.name.hasError('pattern') ? 'No es un nombre válido' : '';
+ }
+
+ getLastNameErrorMessage() {
+   if (this.lastname.hasError('required')) {
+    return 'Debes ingresar un apellido';
+  }
+
+  return this.name.hasError('pattern') ? 'Solo se permiten letras y espacios' : '';
+}
+
+
+//   getErrorMessage() {
+//     if (this.email.hasError('required')) {
+//       return 'Debes ingresar un valor';
+//     }
+
+//     return this.email.hasError('email') ? 'No es un correo electronico valido' : '';
+//   }
+
+  
+
+//    checkPasswords() { 
+//     this.password.value === this.confirmpassword.value ? 'Las contraseñas no coinciden' : '';    
+// }
+
+
+
+
 
   constructor() { }
 
