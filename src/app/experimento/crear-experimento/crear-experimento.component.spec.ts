1f3542851afb51080e4dbc2ae54411cfcd72e578
DanielCarrami <danielcarrami@gmail.com>
2020-12-01 20:12:44 -0600

    Tests fixed

--- src/app/experimento/crear-experimento/crear-experimento.component.spec.ts
+++ src/app/experimento/crear-experimento/crear-experimento.component.spec.ts
@@ -3,23 +3,4 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CrearExperimentoComponent } from './crear-experimento.component';
 
 describe('CrearExperimentoComponent', () => {
-  let component: CrearExperimentoComponent;
-  let fixture: ComponentFixture<CrearExperimentoComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CrearExperimentoComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CrearExperimentoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
