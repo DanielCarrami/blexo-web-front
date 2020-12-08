d1dd00c9ae84b879bf5be70f729f515a754ff5aa
PatoFb <spfautschb@gmail.com>
2020-12-08 14:15:23 -0600

    Project tests added

--- src/app/proyecto/create-project/create-project.component.spec.ts
+++ src/app/proyecto/create-project/create-project.component.spec.ts
@@ -5,29 +5,51 @@ import { CreateProjectComponent } from './create-project.component';
 
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule} from '@angular/common/http/testing';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ApolloTestingModule, ApolloTestingController } from 'apollo-angular/testing';
 
 describe('CreateProjectComponent', () => {
+    let controller: ApolloTestingController;
     let component: CreateProjectComponent;
     let fixture: ComponentFixture<CreateProjectComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
         imports: [
+            RouterTestingModule,
             HttpClientTestingModule,
-            RouterTestingModule
+            ApolloTestingModule
+         ],
+         providers: [
+            { provide: MAT_DIALOG_DATA, useValue: {} },
+            { provide: MatDialogRef, useValue: {} },
+
         ],
-      declarations: [ CreateProjectComponent ]
-    })
-    .compileComponents();
-  }));
+        declarations: [ CreateProjectComponent ]
+      })
+      .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CreateProjectComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+          imports: [ApolloTestingModule],
+        });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        controller = TestBed.get(ApolloTestingController);
+      });
+
+      afterEach(() => {
+        controller.verify();
+      });
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(CreateProjectComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      controller = TestBed.get(ApolloTestingController);
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
 });
